22284956f82fa9e1fddb6ec72ab4c4b6b74e4dfe
bjoernkneisel <info@bjoern-kneisel.de>
2021-05-26 10:45:04 +0200

    Responsive Design Fixes

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,58 +1,49 @@
 <template>
-  <div class="columns is-centered is-vcentered">
-    <div class="column is-12-desktop is-two-thirds-widescreen is-half-fullhd">
-      <form class="box" @submit.prevent="Login">
-        <p class="is-size-3 mb-4 has-text-centered">Anmeldung</p>
-        <div class="field">
-          <p class="control has-icons-left">
-            <input
-              class="input"
-              type="email"
-              placeholder="Email"
-              v-model="email"
-            />
-            <span class="icon is-small is-left">
-              <i class="fas fa-envelope"></i>
-            </span>
-          </p>
-        </div>
-        <div class="field">
-          <p class="control has-icons-left">
-            <input
-              class="input"
-              :type="passwordFieldType"
-              placeholder="Password"
-              v-model="password"
-            />
-            <span class="icon is-small is-left">
-              <i class="fas fa-lock"></i>
-            </span>
-          </p>
-        </div>
-        <nav class="level">
-          <div class="level-left">
-            <div class="level-item">
-              <label class="checkbox">
-                <input type="checkbox" @click="switchVisibility()"/>
-                Passwort anzeigen
-              </label>
-            </div>
-          </div>
-        </nav>
-
-        <p class="control">
-          <button
-            class="button is-primary"
-            style="width: 100%"
-            :class="{ 'is-loading': loading }"
-            @click="login()"
-          >
-            Login
-          </button>
-        </p>
-      </form>
+  <form class="box" @submit.prevent="Login">
+    <p class="is-size-3 mb-4 has-text-centered">Anmeldung</p>
+    <div class="field">
+      <p class="control has-icons-left">
+        <input class="input" type="email" placeholder="Email" v-model="email" />
+        <span class="icon is-small is-left">
+          <i class="fas fa-envelope"></i>
+        </span>
+      </p>
     </div>
-  </div>
+    <div class="field">
+      <p class="control has-icons-left">
+        <input
+          class="input"
+          :type="passwordFieldType"
+          placeholder="Password"
+          v-model="password"
+        />
+        <span class="icon is-small is-left">
+          <i class="fas fa-lock"></i>
+        </span>
+      </p>
+    </div>
+    <nav class="level">
+      <div class="level-left">
+        <div class="level-item">
+          <label class="checkbox">
+            <input type="checkbox" @click="switchVisibility()" />
+            Passwort anzeigen
+          </label>
+        </div>
+      </div>
+    </nav>
+
+    <p class="control">
+      <button
+        class="button is-primary"
+        style="width: 100%"
+        :class="{ 'is-loading': loading }"
+        @click="login()"
+      >
+        Login
+      </button>
+    </p>
+  </form>
 </template>
 
 <script>
@@ -84,7 +75,8 @@ export default {
         });
     }
     function switchVisibility() {
-      passwordFieldType.value = this.passwordFieldType === "password" ? "text" : "password";
+      passwordFieldType.value =
+        this.passwordFieldType === "password" ? "text" : "password";
     }
 
     return {
@@ -93,7 +85,7 @@ export default {
       password,
       login,
       loading,
-      switchVisibility
+      switchVisibility,
     };
   },
 };
