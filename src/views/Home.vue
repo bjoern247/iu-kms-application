90da3ecd751bfaae0a801e06fc3c5d0de3d69084
bjoernkneisel <info@bjoern-kneisel.de>
2021-07-14 16:26:44 +0200

    Added Logout Button for deactivated users

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,37 @@
 <template>
-  <div v-if="userData.appLoaded" class="home">
+  <div v-if="userData.deactivated" class="columns is-centered is-vcentered">
+    <div
+      class="
+        column
+        is-12-desktop is-two-thirds-widescreen is-half-fullhd is-12-mobile
+      "
+    >
+      <div class="box has-text-centered">
+        <h1 style="is-size-2">
+          Dein Account wurde deaktiviert, auf Grund missbräuchlichen Verhaltens
+        </h1>
+        <div class="field mt-2">
+          <p class="control">
+            <a class="button is-danger is-fullwidth" @click="logout()"
+              >Logout</a
+            >
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div v-else-if="userData.appLoaded" class="home">
     <StudentView v-if="userData.role === 'student'" />
     <EditorView v-else-if="userData.role === 'editor'" />
     <AdminView v-else-if="userData.role === 'admin'" />
   </div>
   <div v-else class="columns is-centered is-vcentered">
-    <div class="column is-12-desktop is-two-thirds-widescreen is-half-fullhd is-12-mobile">
+    <div
+      class="
+        column
+        is-12-desktop is-two-thirds-widescreen is-half-fullhd is-12-mobile
+      "
+    >
       <div class="box has-text-centered">
         <div class="lds-roller">
           <div></div>
@@ -26,7 +52,7 @@
 import StudentView from "./student/StudentView";
 import EditorView from "./editor/EditorView";
 import AdminView from "./admin/AdminView";
-import { getUserData } from "../store/firebase";
+import useFirebaseAuth, { getUserData } from "../store/firebase";
 export default {
   components: {
     StudentView,
@@ -35,8 +61,10 @@ export default {
   },
   setup() {
     const userData = getUserData();
+    const { logout } = useFirebaseAuth();
     return {
       userData,
+      logout
     };
   },
 };
