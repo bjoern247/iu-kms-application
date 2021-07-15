930d6d94f77e30a7f90c35c2b071be806df265dc
bjoernkneisel <info@bjoern-kneisel.de>
2021-07-15 10:31:01 +0200

    Added filtering and listener management for atev

--- src/views/editor/MyTickets.vue
+++ src/views/editor/MyTickets.vue
@@ -81,32 +81,32 @@
             <span class="tag is-medium is-info">{{ ticket.courseId }} </span>
           </div>
           <div
-          class="
-            column
-            is-2-desktop is-2-tablet is-2-widescreen is-3-fullhd is-3-mobile
-          "
-        >
-          <span
-                  v-if="ticket.ticketStatus === 'created'"
-                  class="tag is-medium is-dark"
-                  >In Prüfung</span
-                >
-                <span
-                  v-else-if="ticket.ticketStatus === 'validated'"
-                  class="tag is-medium is-success"
-                  >In Bearbeitung</span
-                >
-                <span
-                  v-else-if="ticket.ticketStatus === 'closed'"
-                  class="tag is-medium is-dark"
-                  >Abgeschlossen</span
-                >
-                <span
-                  v-else-if="ticket.ticketStatus === 'awaiting deletion'"
-                  class="tag is-medium is-danger"
-                  >Löschung</span
-                >
-        </div>
+            class="
+              column
+              is-2-desktop is-2-tablet is-2-widescreen is-3-fullhd is-3-mobile
+            "
+          >
+            <span
+              v-if="ticket.ticketStatus === 'created'"
+              class="tag is-medium is-dark"
+              >In Prüfung</span
+            >
+            <span
+              v-else-if="ticket.ticketStatus === 'validated'"
+              class="tag is-medium is-success"
+              >In Bearbeitung</span
+            >
+            <span
+              v-else-if="ticket.ticketStatus === 'closed'"
+              class="tag is-medium is-dark"
+              >Abgeschlossen</span
+            >
+            <span
+              v-else-if="ticket.ticketStatus === 'awaiting deletion'"
+              class="tag is-medium is-danger"
+              >Löschung</span
+            >
+          </div>
           <div
             class="
               column
@@ -119,7 +119,15 @@
             "
           >
             <span>
-              {{ ticket.datetime.toDate().toLocaleDateString("de-DE", {day: "numeric", year: "numeric", month: "2-digit", hour: "numeric", minute: "numeric"}) }}
+              {{
+                ticket.datetime.toDate().toLocaleDateString("de-DE", {
+                  day: "numeric",
+                  year: "numeric",
+                  month: "2-digit",
+                  hour: "numeric",
+                  minute: "numeric",
+                })
+              }}
             </span>
           </div>
           <div
@@ -140,34 +148,66 @@
             </div>
           </div>
         </div>
-        <hr class="m-0">
+        <hr class="m-0" />
       </div>
       <hr class="mb-0" />
       <p class="panel-tabs">
-        <a class="is-active">Alle</a>
-        <a>In Bearbeitung</a>
-        <a>Abgeschlossen</a>
+        <a :class="{ 'is-active': showingAll }" @click="showAll()">Alle</a>
+        <a :class="{ 'is-active': showingValidated }" @click="showValidated()"
+          >In Bearbeitung</a
+        >
+        <a :class="{ 'is-active': showingClosed }" @click="showClosed()"
+          >Abgeschlossen</a
+        >
       </p>
     </div>
   </nav>
 </template>
 
 <script>
-import useFirebase, { getAssignedTickets } from "../../store/firebase";
+import useFirebase, { getAssignedTickets, loadAssignedTickets, loadAssignedTicketsFilterClosed, loadAssignedTicketsFilterValidated } from "../../store/firebase";
 import { useRouter } from "vue-router";
+import { ref } from "vue";
 export default {
   setup() {
     const tickets = getAssignedTickets();
     const router = useRouter();
     const state = useFirebase();
     const userData = state.userData.value;
+    const showingAll = ref(true);
+    const showingValidated = ref(false);
+    const showingClosed = ref(false);
     function showTicket(id) {
-      router.push('/ticket-detail-view/' + id);
+      router.push("/ticket-detail-view/" + id);
     }
+    const showValidated = () => {
+      showingAll.value = false;
+      showingValidated.value = true;
+      showingClosed.value = false;
+      loadAssignedTicketsFilterValidated();
+    };
+    const showClosed = () => {
+      showingAll.value = false;
+      showingValidated.value = false;
+      showingClosed.value = true;
+      loadAssignedTicketsFilterClosed();
+    };
+    const showAll = () => {
+      showingAll.value = true;
+      showingValidated.value = false;
+      showingClosed.value = false;
+      loadAssignedTickets();
+    };
     return {
       tickets,
       userData,
-      showTicket
+      showTicket,
+      showingAll,
+      showingValidated,
+      showingClosed,
+      showValidated,
+      showClosed,
+      showAll,
     };
   },
 };
